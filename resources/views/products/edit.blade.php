b46a3e775f0bb91dccdfb6d1a16a1019bded0807
Minul Hemsara <codse211f033@student.nibm.lk>
2023-09-20 13:20:08 +0530

    edit.blade.php

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -4,6 +4,55 @@
     </div>
 @endif
 
+<style>
+    /* Style the form container */
+    .form-container {
+        width: 50%;
+        margin: 0 auto;
+    }
+
+    /* Style form elements */
+    form {
+        background-color: #f2f2f2;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    label {
+        display: block;
+        margin-bottom: 5px;
+        font-weight: bold;
+    }
+
+    input[type="text"],
+    textarea,
+    select {
+        width: 100%;
+        padding: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+
+    select {
+        height: 40px;
+    }
+
+    button[type="submit"] {
+        padding: 10px 20px;
+        background-color: #007bff;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    button[type="submit"]:hover {
+        background-color: #0056b3;
+    }
+</style>
+
+
 <h1>Edit Product</h1>
 
 <form method="POST" action="{{ route('products.update', $product) }}">
